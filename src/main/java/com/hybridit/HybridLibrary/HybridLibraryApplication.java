6e4db3c4616609d84e75df0372b480794101c0dc
Sara Rat <sara.rat.ns.90@gmail.com>
2019-06-21 09:54:03 +0200

    pom modified

--- src/main/java/com/hybridit/HybridLibrary/HybridLibraryApplication.java
+++ src/main/java/com/hybridit/HybridLibrary/HybridLibraryApplication.java
@@ -3,12 +3,15 @@ package com.hybridit.HybridLibrary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Clock;
+
 @SpringBootApplication
 public class HybridLibraryApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(HybridLibraryApplication.class, args);
+		System.out.println("Sara");
 	}
 
 }

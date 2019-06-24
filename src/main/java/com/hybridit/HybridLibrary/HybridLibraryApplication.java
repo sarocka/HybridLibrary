e73d4d8a812e79e3139efe8f21d25a26fd7f0c5b
Sara Rat <sara.rat.ns.90@gmail.com>
2019-06-24 10:32:42 +0200

    model class Book created

--- src/main/java/com/hybridit/HybridLibrary/HybridLibraryApplication.java
+++ src/main/java/com/hybridit/HybridLibrary/HybridLibraryApplication.java
@@ -1,5 +1,7 @@
 package com.hybridit.HybridLibrary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +12,8 @@ public class HybridLibraryApplication {
 
 	public static void main(String[] args) {
 
+		Logger logger = LoggerFactory.getLogger(HybridLibraryApplication.class);
+		logger.info("test");
 		SpringApplication.run(HybridLibraryApplication.class, args);
 
 	}
---
+++ src/main/java/com/hybridit/HybridLibrary/model/Book.java
@@ -0,0 +1,27 @@
+package com.hybridit.HybridLibrary.model;
+
+public class Book {
+    private Long id;
+    private long title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getTitle() {
+        return title;
+    }
+
+    public void setTitle(long title) {
+        this.title = title;
+    }
+
+    
+
+
+
+}

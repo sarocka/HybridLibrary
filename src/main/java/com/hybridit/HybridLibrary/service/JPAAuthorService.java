b6ca3300ce852201e6c45484b1a55e79b7e5f930
Sara Rat <sara.rat.ns.90@gmail.com>
2019-06-28 15:19:12 +0200

    Service class and tests for author created

---
+++ src/main/java/com/hybridit/HybridLibrary/service/JPAAuthorService.java
@@ -0,0 +1,54 @@
+package com.hybridit.HybridLibrary.service;
+
+import com.hybridit.HybridLibrary.model.Author;
+import com.hybridit.HybridLibrary.repository.AuthorRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Service
+public class JPAAuthorService implements AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public JPAAuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public Author findOne(Long id) {
+        return authorRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "the entity with a given id does not exist"));
+    }
+
+    @Override
+    public List<Author> findAll() {
+        List<Author>authors= authorRepository.findAll();
+        if(authors.isEmpty()){
+           throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No authors to display");
+        }
+        return authors;
+    }
+
+    @Override
+    public Author save(Author author) {
+        return authorRepository.save(author);
+    }
+
+    @Override
+    public Author delete(Long id) {
+        return authorRepository.findById(id).
+                map(author -> {authorRepository.delete(author);
+                    return author;})
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Author with provided id does not exist"));
+    }
+
+    @Override
+    public Author update(Author author, Long id) {
+        if(authorRepository.existsById(id)){
+          return authorRepository.save(author);
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Author with provided id does not exist");
+    }
+}
---
+++ src/test/java/com/hybridit/HybridLibrary/service/JPAAuthorServiceTest.java
@@ -0,0 +1,108 @@
+package com.hybridit.HybridLibrary.service;
+
+import com.hybridit.HybridLibrary.model.Author;
+import com.hybridit.HybridLibrary.repository.AuthorRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class JPAAuthorServiceTest {
+
+    @InjectMocks
+    JPAAuthorService jpaAuthorService;
+    @Mock
+    AuthorRepository authorRepositoryMock;
+
+    @Test
+    public void findOne_existingId_authorReturned() {
+        Author authorFromDb = new Author();
+
+        when(authorRepositoryMock.findById(authorFromDb.getId())).thenReturn(Optional.of(authorFromDb));
+        Author authorReturned = jpaAuthorService.findOne(authorFromDb.getId());
+
+        assertNotNull(authorReturned);
+        assertEquals(authorFromDb, authorReturned);
+    }
+
+    @Test(expected = ResponseStatusException.class)
+    public void findOne_nonExistingId_execptionThrown() {
+
+        when(authorRepositoryMock.findById(anyLong())).thenReturn(Optional.empty());
+        jpaAuthorService.findOne(anyLong());
+    }
+
+    @Test
+    public void findAll_authorsExistingInDb_listReturned() {
+        List<Author> authorsFromDb = new ArrayList<>();
+        Author authorFromDb = new Author();
+        authorFromDb.setId(1L);
+        authorsFromDb.add(authorFromDb);
+
+        when(authorRepositoryMock.findAll()).thenReturn(authorsFromDb);
+
+        List<Author> authorsReturned = jpaAuthorService.findAll();
+
+        assertFalse(authorsReturned.isEmpty());
+        assertEquals(authorsFromDb.size(), authorsReturned.size());
+        assertEquals(authorsFromDb.get(0), authorsReturned.get(0));
+    }
+
+    @Test(expected = ResponseStatusException.class)
+    public void findAll_noAuthorsToDisplay_ExceptionThrown() {
+        when(authorRepositoryMock.findAll()).thenReturn(Collections.EMPTY_LIST);
+        jpaAuthorService.findAll();
+    }
+
+    @Test
+    public void delete_existingIdProvided_deletedAuthorReturned() {
+        Author authorFromDb = new Author();
+
+        when(authorRepositoryMock.findById(authorFromDb.getId())).thenReturn(Optional.of(authorFromDb));
+        Author authorDeleted = jpaAuthorService.delete(authorFromDb.getId());
+
+        verify(authorRepositoryMock).delete(authorFromDb);
+
+        assertEquals(authorFromDb, authorDeleted);
+    }
+
+    @Test (expected = ResponseStatusException.class)
+    public void delete_nonexistingIdProvided_exceptionThrown() {
+
+        when(authorRepositoryMock.findById(anyLong())).thenReturn(Optional.empty());
+        jpaAuthorService.delete(anyLong());
+    }
+    @Test
+    public void update_existingIdProvided_updatedAuthorReturned(){
+        Author authorForUpdate= new Author();
+
+        when(authorRepositoryMock.existsById(anyLong())).thenReturn(true);
+        when(authorRepositoryMock.save(authorForUpdate)).thenReturn(authorForUpdate);
+
+       Author updatedAuthor= jpaAuthorService.update(authorForUpdate,1L);
+
+       assertEquals(authorForUpdate,updatedAuthor);
+    }
+    @Test(expected = ResponseStatusException.class)
+    public void update_nonexistingIdProvided_exceptionThrown(){
+
+        when(authorRepositoryMock.existsById(anyLong())).thenReturn(false);
+        jpaAuthorService.update(new Author(),1L);
+
+
+    }
+
+}

6896e2da5b8fc5a435efe58098bb7253f1e7f9d7
Sara Rat <sara.rat.ns.90@gmail.com>
2019-07-11 14:29:17 +0200

    service layer created

---
+++ src/main/java/com/hybridit/HybridLibrary/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.hybridit.HybridLibrary.service;
+
+import com.hybridit.HybridLibrary.model.Author;
+import com.hybridit.HybridLibrary.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    Customer findOne(Long id);
+    List<Customer> findAll();
+    Customer save(Customer customer);
+    Customer delete(Long id);
+    Customer update(Customer customer, Long id);
+}

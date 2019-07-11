fe56c705aaee0a0aadd57f640b20f5f8b6ddcd0f
Sara Rat <sara.rat.ns.90@gmail.com>
2019-07-11 14:20:52 +0200

    customer repository created

---
+++ src/main/java/com/hybridit/HybridLibrary/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.hybridit.HybridLibrary.repository;
+
+import com.hybridit.HybridLibrary.model.Author;
+import com.hybridit.HybridLibrary.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}

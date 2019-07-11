4158ec56c3ac69e8fc91bbffb42a21b6772893b7
Sara Rat <sara.rat.ns.90@gmail.com>
2019-07-11 16:00:33 +0200

    removed commented code

--- src/main/java/com/hybridit/HybridLibrary/controller/CustomerController.java
+++ src/main/java/com/hybridit/HybridLibrary/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.hybridit.HybridLibrary.model.Customer;
 import com.hybridit.HybridLibrary.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,31 +19,31 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    //@Secured({"MANAGER"})
+    @Secured({"MANAGER", "LIBRARIAN"})
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Customer>> getAll() {
         return new ResponseEntity<>(customerService.findAll(), HttpStatus.OK);
     }
 
-    // @Secured({"MANAGER"})
+    @Secured({"MANAGER", "LIBRARIAN"})
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     public ResponseEntity<Customer> getOne(@PathVariable Long id) {
         return new ResponseEntity<>(customerService.findOne(id), HttpStatus.OK);
     }
 
-    // @Secured({"MANAGER"})
+    @Secured({"MANAGER", "LIBRARIAN"})
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
     public ResponseEntity<Customer> delete(@PathVariable Long id) {
         return new ResponseEntity<>(customerService.delete(id), HttpStatus.OK);
     }
 
-    // @Secured({"MANAGER"})
+    @Secured({"MANAGER", "LIBRARIAN"})
     @RequestMapping(method = RequestMethod.POST, consumes = "application/json")
     public ResponseEntity<Customer> create(@RequestBody Customer customer) {
         return new ResponseEntity<>(customerService.save(customer), HttpStatus.CREATED);
     }
 
-    //  @Secured({"MANAGER"})
+    @Secured({"MANAGER", "LIBRARIAN"})
     @RequestMapping(method = RequestMethod.PUT, consumes = "application/json", value = "/{id}")
     public ResponseEntity<Customer> update(@RequestBody Customer customer, @PathVariable Long id) {
         return new ResponseEntity<>(customerService.update(customer, id), HttpStatus.OK);
